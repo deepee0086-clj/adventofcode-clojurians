6cd1602597c16aa024148ebe6019b398c7d1c485
indigo0086 <indigo0086@gmail.com>
2017-12-03 23:58:26 -0800

    Day 2 complete

--- src/day_02.clj
+++ src/day_02.clj
@@ -1,9 +1,11 @@
 (ns day-02
   (:require utils
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [inputs :refer [day-02] :rename {day-02 input}]))
 
 (defn- str->row [row]
-  (->> (str/split row #"\s")
+  (->> (str/split row #"\s+")
+       (filter (complement empty?))
        (map #(Integer/parseInt %))))
 
 (defn- str->spreadsheet [input]
@@ -19,7 +21,7 @@
 
 (defn- check-max [candidate max] (check-bounds candidate max >))
 
-(defn row-checksum [spreadsheet-row]
+(defn row-checksum-min-max [spreadsheet-row]
   (loop [[first & rest] spreadsheet-row
          min nil
          max nil]
@@ -27,8 +29,34 @@
       (- max min)
       (recur rest (check-min first min) (check-max first max)))))
 
+(defn- is-divisible? [number other-number]
+  (= 0 (if (< number other-number)
+         (mod other-number number)
+         (mod number other-number))))
+
+(defn- find-divisible-number
+  [item others]
+  (let [filter-fn (partial is-divisible? item)]
+    (->> others
+         (filter filter-fn)
+         first)))
+
+(defn row-checksum-divisible [spreadsheet-row]
+  (println spreadsheet-row)
+  (loop [[cur-cell & rest] spreadsheet-row
+         dividend 0
+         divisor 0]
+    (cond
+      (nil? cur-cell) (/ dividend divisor)
+      :else (let [div-number (find-divisible-number cur-cell rest)]
+              (if (nil? div-number)
+                (recur rest dividend divisor)
+                (recur nil (max cur-cell div-number) (min cur-cell div-number)))))))
+
 (defn get-checksum [input]
   (let [spreadsheet (str->spreadsheet input)]
     (->> spreadsheet
-         (map row-checksum)
+         (map row-checksum-divisible)
          (reduce +))))
+
+(get-checksum input)
