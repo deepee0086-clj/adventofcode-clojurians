b4083bfa82a627f8cad04a5ea06736622fd3117a
indigo0086 <indigo0086@gmail.com>
2017-12-25 19:15:36 -0800

    Closer to day 3 pt 2

--- src/day_03.clj
+++ src/day_03.clj
@@ -1,8 +1,8 @@
 (ns day-03
-  (:require utils
-            [clojure.string :as str]
-            [clojure.set :as cset]
-            [inputs :refer [day-03] :rename {day-03 input}]))
+  (:require
+   [clojure.string :as str]
+   [clojure.set :as cset]
+   [inputs :refer [day-03] :rename {day-03 input}]))
 
 (def base-adjacent-nums 8)
 
@@ -57,11 +57,55 @@
   "Give a num, return how many steps to return to 1 in spiral sequence"
   [num]
   (cond
-    (< num 1) nil
     (= 1 num) 0
-    :else (let [{:keys [level offset]} (get-level num)
-                [min max] (moves-range level)
-                step-sequence (frequency-sequence min max (dec max))]
-            (nth step-sequence offset))))
+    (> num 1) (let [{:keys [level offset]} (get-level num)
+                    [min max] (moves-range level)
+                    step-sequence (frequency-sequence min max (dec max))]
+                (nth step-sequence offset))))
 
-(steps-to-origin inputs)
+(steps-to-origin input)
+(time (steps-to-origin input))
+
+#_(level-side-length 0)
+;; Part 2
+(defn- offset-coord [[coord-x coord-y] [offset-x offset-y]]
+  [(+ coord-x offset-x) (+ coord-y offset-y)])
+
+(def neighbor-offsets [[0 1] [1 1] [1 0] [1 -1] [0 -1] [-1 -1] [-1 0] [-1 1]])
+
+(defn- add-surrounding [coord grid]
+  (reduce
+   (fn [sum offset]
+     (if-let [neighbor-val (grid (offset-coord coord offset))]
+       (+ sum neighbor-val)
+       sum))
+   0
+   neighbor-offsets))
+
+#_(add-surrounding [0 1] {[0 0] 1
+                          [1 0] 1
+                          [1 1] 2})
+
+(defn traverse-grid
+  [n]
+  (loop [grid {[0 0] 1} ;starting grid to use for
+         last-val 1
+         cur-coord [1 0]
+         idx 0
+         [cur-offset & rest-offset :as offsets] [[1 0] [0 1] [-1 0] [0 -1]]]
+    (if (= current-index n) last-val
+        (let [surrounding-sum (add-surrounding (:coord cur-item))]
+          (recur (assoc grid cur-coord surrunding-sum)
+                 surrounding-sum
+                 (offset-coord cur-coord cur-offset)
+                 (inc x))))))
+
+
+#_(loop [[first & rest] '(1 2 3)
+         accum 0]
+    (if (nil? first)
+      accum
+      (recur rest (+ accum first))))
+;;
+#_(let [[first & rest] [[1 0] [0 1] [-1 0] [0 -1]]]
+    (conj (into [] rest) first))
